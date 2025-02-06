c44dd5fff148703a74a32b92c83c094645c966fd
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-06 15:29:34 +0530

    Time: 214 ms (60.90%) | Memory: 68.2 MB (79.49%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.java
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -4,10 +4,9 @@ class Solution {
         Map<Integer,Integer>mp=new HashMap<>();
 
         int n=nums.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
 
-                if(i==j) continue;
                 int product = nums[i] * nums[j];
                 mp.put(product, mp.getOrDefault(product, 0) + 1);
             }
@@ -15,8 +14,8 @@ class Solution {
         int pairs=0;
         for(int value:mp.values()){
             int curr_value=0;
-            if(value>=4){
-                curr_value=value/2;
+            if(value>=2){
+                curr_value=value;
                 
                 pairs+=(curr_value-1)*(curr_value)/2;
             
