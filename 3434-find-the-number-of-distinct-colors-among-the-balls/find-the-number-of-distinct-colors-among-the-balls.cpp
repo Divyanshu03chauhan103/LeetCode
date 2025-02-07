83ea0aebf527ef85dbe8e8891ad5d013590aa46c
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-07 11:46:58 +0530

    Time: 183 ms (31.83%) | Memory: 171.8 MB (20.90%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -4,7 +4,7 @@ public:
       vector<int> result;
         unordered_map<int, int> mp; 
         unordered_map<int, int> freq; 
-        unordered_set<int> s1;  
+        // unordered_set<int> s1;  
         
         for (vector<int> i : queries) {
             int key = i[0], value = i[1];
@@ -13,15 +13,15 @@ public:
             if (mp.find(key) != mp.end()) {
                 int oldValue = mp[key];
                 freq[oldValue]--;
-                if (freq[oldValue] == 0) s1.erase(oldValue);
+                if (freq[oldValue] == 0) freq.erase(oldValue);
             }
 
            
             mp[key] = value;
             freq[value]++;
-            s1.insert(value);
+            // s1.insert(value);
 
-            result.push_back(s1.size());
+            result.push_back(freq.size());
         }
 
         return result;
