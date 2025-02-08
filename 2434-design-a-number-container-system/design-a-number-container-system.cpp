6b18c79948231e1ac88e3dfbd007933ff2dc087e
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-08 14:28:54 +0530

    Time: 103 ms (79.06%) | Memory: 185.6 MB (93.13%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,31 +1,49 @@
 class NumberContainers {
 public:
     unordered_map<int,int>mp;
-    unordered_map<int,set<int>>mp1;
+    // unordered_map<int,set<int>>mp1;
+    unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mp1;
     NumberContainers() {
        
     }
     
     void change(int index, int number) {
         
-        if(mp.find(index)!=mp.end()){
-            int value = mp[index];
-            mp1[value].erase(index);
-            if(mp1[value].empty())
-                mp1.erase(value);
-        }
+        // if(mp.find(index)!=mp.end()){
+        //     int value = mp[index];
+        //     mp1[value].erase(index);
+        //     if(mp1[value].empty())
+        //         mp1.erase(value);
+        // }
+        // mp[index]=number;
+        // mp1[number].insert(index);
+
         mp[index]=number;
-        mp1[number].insert(index);
+        mp1[number].push(index);
     }
     
     int find(int number) {
         
-        int index=-1;
+        // int index=-1;
 
-        if(mp1.find(number)!=mp1.end())
-            index=*mp1[number].begin();
+        // if(mp1.find(number)!=mp1.end())
+        //     index=*mp1[number].begin();
 
-        return index;
+        // return index;
+
+        if(mp1.find(number)==mp1.end()) return -1;
+
+        auto &pq = mp1[number];
+
+        while(!pq.empty()){
+
+            int index=pq.top();
+
+            if(mp[index]==number) return index;
+            pq.pop();
+        }
+
+        return -1;
     }
 };
 
