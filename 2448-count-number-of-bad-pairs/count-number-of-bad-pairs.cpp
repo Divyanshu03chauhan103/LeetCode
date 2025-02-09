a5187f31207969cc44df8e69815db5eeeb25e4c9
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-09 13:42:49 +0530

    Time: 71 ms (47.87%) | Memory: 87.6 MB (70.06%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        unordered_map<long long, long long> mp;
-        for(int i = 0; i < nums.size(); i++) {
-            long long value = nums[i] - i;
-            mp[value]++;
+        // unordered_map<long long, long long> mp;
+        // for(int i = 0; i < nums.size(); i++) {
+        //     long long value = nums[i] - i;
+        //     mp[value]++;
+        // }
+
+        // long long good_pairs = 0;
+        // for(auto &[key, val] : mp) {
+        //     if(val > 1) {
+        //         good_pairs += (val * (val - 1)) / 2;  
+        //     }
+        // }
+
+        // long long n = nums.size();
+        // long long total_pairs = (n * (n - 1)) / 2;
+
+        // return total_pairs - good_pairs;
+
+        unordered_map<int,int>mp;
+        long long result=0;
+
+        for(int i=0;i<nums.size();i++){
+
+            int good_pairs=mp[nums[i]-i];
+
+            int total_pairs=i;
+
+            result+=total_pairs-good_pairs;
+
+            mp[nums[i]-i]++;
         }
-
-        long long good_pairs = 0;
-        for(auto &[key, val] : mp) {
-            if(val > 1) {
-                good_pairs += (val * (val - 1)) / 2;  
-            }
-        }
-
-        long long n = nums.size();
-        long long total_pairs = (n * (n - 1)) / 2;
-
-        return total_pairs - good_pairs;
+        return result;
     }
 };
