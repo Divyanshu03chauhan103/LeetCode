c77ecee2efb1ddb9124870d863bb08916728a01d
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-11 13:05:18 +0530

    Time: 2 ms (40.90%) | Memory: 9.4 MB (34.81%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+        while(true){
+
+            int index=s.find(part);
+
+            if(index==string::npos)
+                break;
+            
+            s.erase(index,part.length());
+        }
+        return s;
+    }
+};
