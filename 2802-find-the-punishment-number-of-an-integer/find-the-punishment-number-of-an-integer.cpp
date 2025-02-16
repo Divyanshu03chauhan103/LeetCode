37bb8fe422f868692ca1f4531b70de8cd1458ed6
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-16 18:13:28 +0530

    Time: 15 ms (76.85%) | Memory: 7.8 MB (99.51%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
-    int punishmentNumber(int n) {
-        int arr[] = { 1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909,
-            918, 945, 964, 990, 991, 999, 1000 };
-        int res = 0;
-        for (int i = 0; i < 29; i++) {
-            if (arr[i] <= n) {
-                res += arr[i] * arr[i];
-            } else {
-                break;
-            }
+/*Approach 1:
+    bool check(string sq,int num,int i,int curr_sum){
+        if(i==sq.length()){
+            return curr_sum==num;
         }
-        return res;
+        if(curr_sum>num) return false;
+
+        bool isPossible=false;
+        for(int j=i;j<sq.length();j++){
+
+            string curr_num = sq.substr(i,j-i+1);
+            int val = stoi(curr_num);
+
+            isPossible=  isPossible || check(sq,num,j+1,curr_sum+val);
+            if(isPossible) return true;
+        }
+        return false;
+    }
+*/
+    bool check(int sq_num,int curr_sum,int num){
+
+        if(sq_num ==0 ){
+            return curr_sum==num;
+        }
+
+        if(curr_sum>num) return false;
+
+        return check( sq_num/10,curr_sum+sq_num%10,num)||
+        check( sq_num/100,curr_sum+sq_num%100,num)||
+        check( sq_num/1000,curr_sum+sq_num%1000,num)||
+        check( sq_num/10000,curr_sum+sq_num%10000,num);
+    }
+    int punishmentNumber(int n) {
+        
+        int result=0;
+        for(int i=1;i<=n;i++){
+
+            int sq_num=i*i;
+            // string sq = to_string(sq_num);
+            // if(check(sq,i,0,0)){ // need to generate all the possible substring and calculate which one is correct
+            //     result = result +sq_num;
+            // }
+            if(check(sq_num,0,i))
+                result+=sq_num;
+        }
+        return result;
     }
 };
