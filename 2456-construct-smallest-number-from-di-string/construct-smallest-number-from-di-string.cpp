47dddc719610be98e8ae37c9d14a73c1f7eeb8c9
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-18 20:14:43 +0530

    Time: 1744 ms (5.07%) | Memory: 283.5 MB (5.27%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
+    bool isValid(string result, string pattern) {
+        for (int i = 0; i < pattern.size(); i++) {
+            if ((pattern[i] == 'I' && result[i] > result[i+1]) ||
+                (pattern[i] == 'D' && result[i] < result[i+1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void solve(vector<string>&permutations,int n,vector<bool>&used,string curr){
+
+        if(curr.size()==n){
+            permutations.push_back(curr);
+            return ;
+        }
+
+        for(int i=1;i<=n;i++){
+
+            if(used[i]) continue;
+
+            used[i] =true;
+            curr.push_back(i+'0');
+            solve(permutations,n,used,curr);
+            used[i]=false;
+            curr.pop_back();
+        }
+
+    }
     string smallestNumber(string pattern) {
         
-        stack<char>st;
-        string result;
-        int counter=1;
-        for(int i =0;i<=pattern.size();i++){
+        int n = pattern.size();
 
-            st.push(counter+'0');
-            if(i == pattern.size() || pattern[i]=='I')
-                while(!st.empty()){
-                    result+=st.top();
-                    st.pop();
-                }
+        vector<bool>used(n+2,false);
 
-            counter++;
+        vector<string>permutations;
+
+        solve(permutations,n+1,used,"");
+        
+        for(auto i:permutations){
+
+            if(isValid(i,pattern)) return i;
         }
-        return result;
+        return "";
     }
 };
