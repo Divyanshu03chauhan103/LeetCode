b197cd8ec16fb37e083b64a5352c2dc1cee6fb81
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-18 20:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (49.49%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,3 +1,28 @@
+//Approach 1:
+
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        
+        stack<char>st;
+        string result;
+        int counter=1;
+        for(int i =0;i<=pattern.size();i++){
+
+            st.push(counter+'0');
+            if(i == pattern.size() || pattern[i]=='I')
+                while(!st.empty()){
+                    result+=st.top();
+                    st.pop();
+                }
+
+            counter++;
+        }
+        return result;
+    }
+};
+
+/* Approach 2:
 class Solution {
 public:
     bool isValid(string result, string pattern) {
@@ -46,3 +71,5 @@ public:
         return "";
     }
 };
+*/
+
