baa901a1bdae6076db374d4f2099a9787170ed41
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-24 18:36:27 +0530

    Time: 285 ms (18.42%) | Memory: 186.8 MB (9.64%) - LeetSync

---
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool DFSBob(unordered_map<int,int>&BobTime,int time,vector<bool>&visited,int bob,unordered_map<int,vector<int>>&adj){
+
+        visited[bob] = true;
+        BobTime[bob] = time;
+        if(bob==0) return true;
+
+        for(auto i : adj[bob]){
+            if(!visited[i]){
+                if(DFSBob(BobTime,time+1,visited,i,adj))   return true;
+            }
+        }
+        BobTime.erase(bob);
+        return false;
+    }
+
+    void alice(int &result,int ans,vector<int>&amount,vector<bool>&visited,int curr,unordered_map<int,vector<int>>&adj,unordered_map<int,int>&BobTime,int t){
+
+        visited[curr]=true;
+        if(BobTime.find(curr)==BobTime.end() || BobTime[curr]>t)
+            ans+=amount[curr];
+        else if(BobTime[curr]==t)
+            ans+=amount[curr]/2;
+        
+        if(adj[curr].size()==1 && curr!=0)
+            result=max(result,ans);
+        for(auto &nbr : adj[curr]){
+            if(!visited[nbr])
+                alice(result,ans,amount,visited,nbr,adj,BobTime,t+1);
+        }
+    }
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        
+        unordered_map<int,vector<int>>adj;
+        unordered_map<int,int>BobTime;
+        int n= amount.size();
+        vector<bool>visited(n,false);
+
+        for(auto i: edges){
+
+            int u = i[0];
+            int v = i[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        DFSBob(BobTime,0,visited,bob,adj);
+
+        int result=INT_MIN;
+
+        visited.assign(n,false);
+        alice(result,0,amount,visited,0,adj,BobTime,0);
+
+        return result;
+    }
+};
