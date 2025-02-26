62e8af74f93561de57fae1d2d58dc101e3f54305
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-26 10:34:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -7,18 +7,17 @@ public:
         int max_sum= INT_MIN;
         int min_sum = INT_MAX;
         int sum = 0;
-
+        int sum2=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
             if(sum<0) sum =0;
             max_sum = max(sum,max_sum);
+
+            sum2+=nums[i];
+            if(sum2>0) sum2=0;
+            min_sum= min(sum2,min_sum);
         }
-        sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum>0) sum=0;
-            min_sum= min(sum,min_sum);
-        }
+       
         return abs(min_sum) > max_sum ? abs(min_sum): max_sum;
     }
 };
