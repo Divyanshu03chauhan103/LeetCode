07272a4f63bca1d9735c1762e88aaa97eea55f6b
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-02-28 14:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (97.60%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,43 +2,35 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         
-        int n= ratings.size();
-        vector<int>left_ngbr(n),right_ngbr(n);
-        left_ngbr[0]=1;
-        int count=1;
-        for(int i=1;i<n;i++){
-            
-            if(ratings[i]>ratings[i-1]){
-                count++;
-                left_ngbr[i]=count;
+        int result=1;
+
+        int i=1;
+        int n =ratings.size();
+        while(i<ratings.size()){
+
+            if(ratings[i]==ratings[i-1]){
+                result++;
+                i++;
+                continue;
             }
-            else{
-
-                count=1;
-                left_ngbr[i]=count;
+            int peek=1;    
+            while(i<n && ratings[i]>ratings[i-1]){
+             
+             peek++;
+             result+= peek;
+             i++;
             }
-        }
-            count=1;
 
-            right_ngbr[n-1]=1;
-         for(int i=n-2;i>=0;i--){
-            
-            if(ratings[i]>ratings[i+1]){
-                count++;
-                right_ngbr[i]=count;
+            int down =1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                result+=down;
+                down++;
+                i++;
             }
-            else{
 
-                count=1;
-                right_ngbr[i]=count;
+            if(down>peek){
+                result+=down-peek;
             }
-        }
-
-        int result=0;
-
-        for(int i=0;i<n;i++){
-
-            result+=max(left_ngbr[i],right_ngbr[i]);
         }
 
         return result;
