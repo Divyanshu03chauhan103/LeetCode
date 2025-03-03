70771875a263b2fe411dc6dc372fd7cd06f2976c
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-03 22:50:39 +0530

    Time: 3 ms (18.18%) | Memory: 182.2 MB (0.00%) - LeetSync

--- 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
+++ 3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
@@ -3,8 +3,11 @@ public:
     int countArrays(vector<int>& original, vector<vector<int>>& bounds) {
         
         int n= original.size();
+    
         int lb=bounds[0][0];
+
         int ub=bounds[0][1];
+
         for(int i=1;i<n;i++){
             int diff = original[i]-original[i-1];
             lb=lb+diff;
