ce9af7491d500a854e39d3a34dbc6419c3d7d2d0
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-03 18:15:23 +0530

    Time: 2 ms (90.85%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
+        vector<int>result(n,0);
+        int left=0;
+        int right = n-1;
+
+        for(int i=0 , j=n-1;i<n;i++,j--){
+
+            if(nums[i]<pivot){
+                result[left]=nums[i];
+                left++;
+            }
+
+            if(nums[j]>pivot){
+                result[right]=nums[j];
+                right--;
+            }
+        }
+
+        while(left<=right){
+            result[left]=pivot;
+            left++;
+        }
+        return result;
+    }
+};
+
+
+//Approach 1:
+/*
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
         vector<int>result;
         int count=0;
         for(int i:nums){
@@ -23,3 +55,4 @@ public:
         return result;
     }
 };
+*/
