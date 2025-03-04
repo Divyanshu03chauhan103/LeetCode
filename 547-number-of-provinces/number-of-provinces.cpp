65739540f2404f248645b8ce85e57281827305d8
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-04 21:58:27 +0530

    Time: 10 ms (10.43%) | Memory: 20.7 MB (31.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -10,22 +10,22 @@ public:
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> adj[n];  // Use 0-based indexing
+        vector<int> adj[n+1]; 
 
-        // Convert adjacency matrix to adjacency list
+       
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i != j && isConnected[i][j]) {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    adj[i+1].push_back(j+1);
+                    adj[j+1].push_back(i+1);
                 }
             }
         }
 
         int count = 0;
-        vector<int> visited(n, 0); // Use correct size
+        vector<int> visited(n+1, 0); 
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n+1; i++) {
             if (!visited[i]) {
                 count++;
                 dfs(visited, adj, i);
