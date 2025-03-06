8aaf0abc0241180bb9bedd064d13aed80c32144d
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-06 10:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (90.26%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         
-        set<int>s;
         int n=grid.size();
         int repeating=-1;
         int sum=0;
+        vector<bool>visited(2501,false);
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-
-                if(s.count(grid[i][j])){
-                    repeating=grid[i][j];
-                }
-                else{
-                    sum+=grid[i][j];
-                    s.insert(grid[i][j]);
-                }
+                    if(!visited[grid[i][j]]){
+                        sum+=grid[i][j];
+                        visited[grid[i][j]]=true;
+                    }
+                    else
+                        repeating = grid[i][j];
+                
             }
         }
 
         int N= n*n;
 
         int missing= N*(N+1)/2-sum;
-
         return {repeating,missing};
     }
 };
