9183211cfd181528e4716e3c1baf133b6fdc9ccb
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-07 15:50:34 +0530

    Time: 346 ms (28.33%) | Memory: 23.2 MB (42.04%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void isPrime(vector<int>&result,int left,int right){
+
+        vector<bool>check(right+1,true);
+
+        for(int i=2;i<=right;i++){
+            
+            if(check[i]){
+                if(i>=left)
+                    result.push_back(i);
+                for(int j=i*2;j<=right;j=j+i){
+                    check[j]=false;
+                }
+            }
+        }
+
+    }
+
+
+    vector<int> closestPrimes(int left, int right) {
+        
+        vector<int>result;
+        isPrime(result,left,right);
+        int num1=-1;
+        int num2=-1;
+        int diff=INT_MAX;
+        for(int i=1;i<result.size();i++){
+
+            if(result[i]-result[i-1]<diff){
+                diff=result[i]-result[i-1];
+                num1=result[i-1];
+                num2=result[i];
+            }
+            else if(result[i]-result[i-1]==diff){
+                if(num1>result[i-1]){
+                    num1=result[i-1];
+                    num2=result[i];
+                }
+            }
+        }
+
+        return {num1,num2};
+    }
+};
