9af97145fac0cb30d9ee44e2c8c346de32c17333
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-24 10:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (59.52%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<int> delr = {-1, 0, +1, 0};
+        vector<int> delc = {0, +1, 0, -1};
+        int iniColor = image[sr][sc];
+
+        
+        if (iniColor != color) {
+            dfs(image, sr, sc, color, iniColor, delr, delc);
+        }
+
+        return image;
+    }
+
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int iniColor,
+             const vector<int>& delr, const vector<int>& delc) {
+        
+        image[sr][sc] = color;  
+
+        int n = image.size();
+        int m = image[0].size();
+        
+        for (int i = 0; i < 4; i++) {
+            int newRow = sr + delr[i];
+            int newCol = sc + delc[i];
+
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m &&
+                image[newRow][newCol] == iniColor) {
+                dfs(image, newRow, newCol, color, iniColor, delr, delc);
+            }
+        }
+    }
+};
