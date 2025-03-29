48099a1b1c12f05e99ee7fd761a29c86cb7fe108
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-29 11:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (39.93%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -25,3 +25,38 @@ public:
         return true;
     }
 };
+//bfs approach
+// class Solution {
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int n=graph.size();
+//         vector<int>sets(n,0);
+
+//         queue<int>q;
+        
+//         for(int i=0;i<n;i++){
+//         if(sets[i]!=0) continue;
+//                 q.push(i);
+//                 sets[i]=1;
+               
+//                 while(!q.empty()){
+
+//                     int node=q.front();
+//                     for(auto i:graph[node]){
+
+//                         if(sets[i]!=0 && sets[node]==sets[i]){
+//                             return false;
+//                         }else if(sets[i]==0){
+
+//                             sets[i]= sets[node]==1 ? -1:1; 
+//                             q.push(i);
+//                         }
+//                     }
+               
+//                     q.pop();
+//                 }
+//         }
+       
+//         return true;
+//     }
+// };
