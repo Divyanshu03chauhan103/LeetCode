d2418b22b76c51ba8bbf72063bdff6d2f2696f5d
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-29 11:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (39.93%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>&graph,vector<int>&sets,int node,int color){
+        sets[node] = color;
+        color = color==1? -1:1;
+        for(auto i:graph[node]){
+
+            if(sets[i]!=0 && sets[i]==sets[node]) return false;
+            else if(sets[i]!=0) continue;
+            else if(dfs(graph,sets,i,color) == false) return false;
+        }
+
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>sets(n,0);
-
-        queue<int>q;
-        
+        int color=1;
         for(int i=0;i<n;i++){
-        if(sets[i]!=0) continue;
-                q.push(i);
-                sets[i]=1;
-               
-                while(!q.empty()){
+            if(sets[i]!=0) continue;
 
-                    int node=q.front();
-                    for(auto i:graph[node]){
-
-                        if(sets[i]!=0 && sets[node]==sets[i]){
-                            return false;
-                        }else if(sets[i]==0){
-
-                            sets[i]= sets[node]==1 ? -1:1; 
-                            q.push(i);
-                        }
-                    }
-               
-                    q.pop();
-                }
+            if(dfs(graph,sets,i,color)==false) return false;
         }
-       
+
         return true;
     }
 };
