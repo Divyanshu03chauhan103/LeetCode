fb907cb62e1ea6e8d929f6eaf3666f732dd03a88
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-03-31 00:24:17 +0530

    Time: 4 ms (55.81%) | Memory: 11.6 MB (16.49%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string RLE(string s){
+        string result="";
+        int count=1;
+        char c=s[0];
+        for(int i=1;i<s.length();i++){
+            if(s[i]==c) count++;
+            else{
+                result+=to_string(count);
+                result.push_back(c);
+                c=s[i];
+                count=1;
+            }
+        }
+
+        result += to_string(count);
+        result.push_back(c);
+
+        return result;
+    }
+    string solve(int n){
+        if(n==1) return "1";
+        
+        string s = solve(n-1);
+        
+        return RLE(s);
+    }
+    string countAndSay(int n) {
+        return solve(n);
+    }
+};
