5cd5ae8dc554c0fa6e8f1e41505c464bcb176bf1
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-04-14 15:54:36 +0530

    Time: 3 ms (97.72%) | Memory: 22.6 MB (55.94%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,36 +1,37 @@
-class DisjointSet {
-public:
-    vector<int> parent, rank;
+class Disjoint{
+    vector<int>parent,rank;
 
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        iota(parent.begin(), parent.end(), 0); // initialize parent[i] = i
-    }
+    public:
+        Disjoint(int n){
+            parent.resize(n+1,0);
+            rank.resize(n+1,0);
 
-    int find(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = find(parent[node]); // path compression
-    }
-
-    void unionByRank(int u, int v) {
-        int pu = find(u);
-        int pv = find(v);
-
-        if (pu == pv) return;
-
-        if (rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        } else if (rank[pv] < rank[pu]) {
-            parent[pv] = pu;
-        } else {
-            parent[pv] = pu;
-            rank[pu]++;
+            for(int i=0;i<n+1;i++){
+                parent[i]=i;
+            }
         }
-    }
-};
 
+        int find(int node){
+            if(node==parent[node]) return node;
+
+            return parent[node]=find(parent[node]);
+        }
+
+        void unionbyrank(int v,int u){
+            int ult_u=find(u);
+            int ult_v=find(v);
+
+            if(rank[ult_u]>rank[ult_v]){
+                parent[ult_v]=ult_u;
+            }
+            else if(rank[ult_u]<rank[ult_v])
+                parent[ult_u]=ult_v;
+            else{
+                parent[ult_u]=ult_v;
+                rank[ult_v]++;
+            }
+        }
+};
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
@@ -41,14 +42,14 @@ public:
         }
 
         // Create DSU for (rows + cols + 1) to avoid overlap
-        DisjointSet dsu(maxRow + maxCol + 2);
+        Disjoint dsu(maxRow + maxCol + 2);
 
         unordered_set<int> uniqueNodes;
 
         for (auto& stone : stones) {
             int nodeRow = stone[0];
             int nodeCol = stone[1] + maxRow + 1; // shift column index to avoid clash with row index
-            dsu.unionByRank(nodeRow, nodeCol);
+            dsu.unionbyrank(nodeRow, nodeCol);
 
             uniqueNodes.insert(nodeRow);
             uniqueNodes.insert(nodeCol);
