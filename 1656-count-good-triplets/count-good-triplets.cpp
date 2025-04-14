6a4d5e79df2b642570334576b72d76cdf012eeb7
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-04-14 13:43:35 +0530

    Time: 9 ms (41.98%) | Memory: 11.2 MB (11.18%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -5,10 +5,13 @@ public:
         int result=0;
         int n=arr.size();
         for(int i=0;i<n-2;i++){
+
             for(int j=i+1;j<n-1;j++){
+                
+                if(abs(arr[i]-arr[j])>a) continue;
                 for(int k=j+1;k<n;k++){
 
-                    if(abs(arr[i]-arr[j])<=a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c ) result++;
+                    if( abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c ) result++;
                 }
             }
         }
@@ -16,3 +19,21 @@ public:
         return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
