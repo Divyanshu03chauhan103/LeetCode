34a849c1e6efb82f2975e64583c16abc907223ab
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-04-16 19:45:57 +0530

    Time: 524 ms (5.03%) | Memory: 21.6 MB (20.76%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>&dp,vector<int>&nums,int i,int n){
-        if(i>=n-1) return 0;
-        if(i==n-2) return dp[i]=1;
-
-        int min_steps= INT_MAX;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=1;j<=nums[i];j++){
-
-            int steps=solve(dp,nums,i+j,n);
-            if(steps!=INT_MAX)
-            min_steps=min(steps+1,min_steps);
-        }
-
-        return dp[i]=min_steps;
-    }
     int jump(vector<int>& nums) {
-        
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(dp,nums,0,n);
+       vector<int>dp(n+1,INT_MAX);
+       dp[0]=0;
+
+       for(int i=1;i<n;i++){
+
+        for(int j=i-1;j>=0;j--){
+            if(j+nums[j]>=i){
+                dp[i]=min(dp[i],dp[j]+1);
+                
+            }
+        }
+       } 
+        return dp[n-1];
     }
 };
