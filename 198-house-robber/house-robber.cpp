005fe1c13ba1ee7d808cb455d7d3349eded2df72
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-04-17 19:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.95%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i,vector<int>&dp) {
-        if (i >= nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        // Option 1: Rob this house and skip the next
-        int rob = nums[i] + solve(nums, i + 2,dp);
+    // int solve(vector<int>& nums, int i,vector<int>&dp) {
+    //     if (i >= nums.size()) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     // Option 1: Rob this house and skip the next
+    //     int rob = nums[i] + solve(nums, i + 2,dp);
 
-        // Option 2: Skip this house
-        int skip = solve(nums, i + 1,dp);
+    //     // Option 2: Skip this house
+    //     int skip = solve(nums, i + 1,dp);
 
-        return dp[i]=max(rob, skip);
-    }
+    //     return dp[i]=max(rob, skip);
+    // }
 
     int rob(vector<int>& nums) {
          vector<int>dp(nums.size(),-1);
-         return solve(nums, 0,dp);
          
+         dp[0]=nums[0];
+         
+         for(int i=1;i<nums.size();i++){
+
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+
+            int non_take=dp[i-1];
+            dp[i]=max(take,non_take);
+         }
+         return dp[nums.size()-1];
     }
 };
