de743a7ce2b8d9a9971db13f1a6909acb1b0c240
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-04-18 00:17:22 +0530

    Time: 12 ms (37.69%) | Memory: 17 MB (27.34%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool solve(string &s,unordered_set<string>&st,vector<int>&dp,int i){
+        if(i>=s.length()) return true;
+
+        if(dp[i]!=-1) return dp[i];
+
+        for(int j=1;i+j<=s.length();j++){
+             
+             string temp=s.substr(i,j);
+             if(st.find(temp)!=st.end() && solve(s,st,dp,i+j)) return dp[i]= true;
+        }
+        return dp[i]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(s.length(),-1);
+        unordered_set<string>st;
+        for(auto i:wordDict){
+            st.insert(i);
+        }
+        return solve(s,st,dp,0);
+    }
+};
