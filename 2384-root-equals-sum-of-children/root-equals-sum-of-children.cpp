6cfe053186edc0fcac73cab7601d6f5db3448123
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-01 18:35:15 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (29.80%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if (!root || !root->left || !root->right)
+            return false;
+        
+        return root->val == root->left->val + root->right->val;
+    }
+};
