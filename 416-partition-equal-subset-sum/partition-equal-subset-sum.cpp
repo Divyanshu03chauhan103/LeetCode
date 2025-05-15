37c7f36e79f58218b52379cdfbd2400e38f41fec
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-15 18:28:23 +0530

    Time: 183 ms (16.07%) | Memory: 17.4 MB (48.18%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    bool f(int i, int n, int target, int partition, vector<int>& nums, vector<vector<int>>& dp) {
-        if (partition > target) return false;
-        if (partition == target) return true;
-        if (i >= n) return false;
-
-        if (dp[i][partition] != -1) return dp[i][partition];
-
-        bool notTake = f(i + 1, n, target, partition, nums, dp);
-        bool take = f(i + 1, n, target, partition + nums[i], nums, dp);
-
-        return dp[i][partition] = notTake || take;
-    }
-
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum % 2 != 0) return false; 
@@ -20,7 +7,24 @@ public:
         int n = nums.size();
         int target = sum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-        return f(0, n, target, 0, nums, dp);
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+
+        for (int i = 0; i < n; ++i) {
+                dp[i][target] = true;
+            }
+
+        for(int i=n-2;i>=0;i--){
+
+            for(int partition=target-1;partition>=0;partition--){
+                
+                
+                bool notTake = dp[i + 1][partition];
+                
+                bool take = (partition+nums[i]<=target) ? dp[i + 1][ partition + nums[i]]:false;
+                dp[i][partition]=notTake||take;
+            }
+        }    
+
+        return dp[0][0];
     }
 };
