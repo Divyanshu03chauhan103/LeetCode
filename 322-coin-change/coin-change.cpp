1a20ca85c7039a2e33e928e0d20d0c857c0b2766
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-16 21:49:35 +0530

    Time: 14 ms (93.69%) | Memory: 17.8 MB (92.22%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int amount, vector<int>& coins, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;
-        if (i >= coins.size()) return INT_MAX - 1;
+    int coinChange(vector<int>& coins, int amount) {
+        const int INF = 1e9;  
+        vector<int> dp(amount + 1, INF);
 
-        if (dp[i][amount] != -1) return dp[i][amount];
+        dp[0] = 0; 
 
-        int notTake = solve(i + 1, amount, coins, dp);
-        int take = INT_MAX;
-        if (coins[i] <= amount) {
-            take = 1 + solve(i, amount - coins[i], coins, dp);
+        for (int coin : coins) {
+            for (int x = coin; x <= amount; ++x) {
+                dp[x] = min(dp[x], 1 + dp[x - coin]);
+            }
         }
 
-        return dp[i][amount] = min(take, notTake);
-    }
-
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-
-        int res = solve(0, amount, coins, dp);
-        return res >= INT_MAX - 1 ? -1 : res;
+        return (dp[amount] == INF) ? -1 : dp[amount];
     }
 };
