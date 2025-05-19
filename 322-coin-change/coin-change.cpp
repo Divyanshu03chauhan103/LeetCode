65e91537c8daf119b8d6db20f291135219653b68
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-19 22:50:46 +0530

    Time: 51 ms (24.94%) | Memory: 49.3 MB (28.18%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        const int INF = 1e9;  
-        vector<int> dp(amount + 1, INF);
+    int solve(int i,int target,vector<vector<int>>&dp,vector<int>&coins){
+        if(target==0) return 0;
+        if(i==0){
+            return target%coins[i]==0? target/coins[i]:1e9;
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notTake = solve(i-1,target,dp,coins);
 
-        dp[0] = 0; 
-
-        for (int coin : coins) {
-            for (int x = coin; x <= amount; ++x) {
-                dp[x] = min(dp[x], 1 + dp[x - coin]);
-            }
+        int take=1e9;
+        if(coins[i]<=target){
+            take = 1+solve(i,target-coins[i],dp,coins);
         }
 
-        return (dp[amount] == INF) ? -1 : dp[amount];
+        return dp[i][target]=min(take,notTake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+
+        int result = solve(n-1,amount,dp,coins);
+
+        if(result>=1e9) return -1;
+        return result;
     }
 };
