9275f7acb434141e887113a934865ce84b5f4f3e
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-20 20:47:19 +0530

    Time: 33 ms (22.01%) | Memory: 46.1 MB (15.41%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int solve(int i,int amount,vector<int>&coins,vector<vector<int>>&dp){
-
-        if(amount==0) return 1;
-        
-        if(i==0){
-            if(amount%coins[i]==0) return 1;
-            else return 0;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int notTake = solve(i-1,amount,coins,dp);
-        int take = 0;
-
-        if(coins[i]<=amount){
-            take = solve(i,amount-coins[i],coins,dp);
-        }
-
-        return dp[i][amount]=take+notTake;
-    }
     int change(int amount, vector<int>& coins) {
-        
+        const int MOD = 1e10 + 7;
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        vector<vector<long long int>> dp(n, vector<long long int>(amount + 1, 0));
 
-        return solve(n-1,amount,coins,dp);
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                long long int notTake = dp[i - 1][j];
+                long long int take = 0;
+                if (coins[i] <= j) {
+                    take = dp[i][j - coins[i]];
+                }
+                dp[i][j] = (take + notTake) % MOD;
+            }
+        }
+
+        return (int)dp[n - 1][amount];
     }
 };
