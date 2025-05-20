8effe9e901d81283cbd0a34fefe1643ce993d2b3
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-05-20 20:49:38 +0530

    Time: 21 ms (47.86%) | Memory: 10.7 MB (76.81%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,27 +3,28 @@ public:
     int change(int amount, vector<int>& coins) {
         const int MOD = 1e10 + 7;
         int n = coins.size();
-        vector<vector<long long int>> dp(n, vector<long long int>(amount + 1, 0));
+        vector<long long int> prev(amount + 1, 0),curr(amount+1,0);
 
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-        }
+        
+            prev[0] = 1;
+        
 
         for (int i = 0; i <= amount; i++) {
-            if (i % coins[0] == 0) dp[0][i] = 1;
+            if (i % coins[0] == 0) prev[i] = 1;
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= amount; j++) {
-                long long int notTake = dp[i - 1][j];
+                long long int notTake = prev[j];
                 long long int take = 0;
                 if (coins[i] <= j) {
-                    take = dp[i][j - coins[i]];
+                    take = curr[j - coins[i]];
                 }
-                dp[i][j] = (take + notTake) % MOD;
+                curr[j] = (take + notTake) % MOD;
             }
+            prev=curr;
         }
 
-        return (int)dp[n - 1][amount];
+        return (int)prev[amount];
     }
 };
