2db60109886dbe8a97d21b7100711f50ac79127d
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-06-01 17:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (26.99%) - LeetSync

---
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distributeCandies(int n, int limit) {
+        int ways=0;
+
+        for(int i=0;i<=min(n,limit);i++){
+
+            for(int j=0;j<=min(limit,n-i);j++){
+
+                int ch3 = min(limit,n-i-j);
+                if(ch3+i+j==n) ways++;
+            }
+        }
+        return ways;
+    }
+};
