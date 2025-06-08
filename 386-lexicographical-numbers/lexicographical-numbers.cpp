24921e8c46492de0f37432252cbcf1bf83c408ca
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-06-08 22:41:38 +0530

    Time: 78 ms (11.28%) | Memory: 28.1 MB (11.28%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        
+        vector<string>v;
+
+        for(int i=1;i<=n;i++){
+
+            v.push_back(to_string(i));
+        }
+
+        sort(v.begin(),v.end());
+
+        vector<int>result;
+
+        for(auto i:v){
+            result.push_back(stoi(i));
+        }
+
+        return result;
+    }
+};
