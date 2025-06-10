ca0321907073e95cffa5ae4ed93179d5e2728989
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-06-10 10:22:22 +0530

    Time: 14 ms (55.89%) | Memory: 164.6 MB (14.17%) - LeetSync

---
+++ 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canMakeEqual(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>minus1 = nums;
+        vector<int>plus1 =nums;
+        int op1=0,op2 = 0;
+        for(int i=0;i<n-1;i++){
+            if(minus1[i]==1){
+                minus1[i]=minus1[i]*-1;
+                minus1[i+1]=minus1[i+1]*-1;
+                op1++;
+            }
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(plus1[i]==-1){
+                plus1[i]=plus1[i]*-1;
+                plus1[i+1]=plus1[i+1]*-1;
+                op2++;
+            }
+        }
+
+        if(plus1[n-1]!=1) op2=1e9;
+        if(minus1[n-1]!=-1) op1=1e9;
+        
+        return min(op1,op2)<=k;
+    }
+};
