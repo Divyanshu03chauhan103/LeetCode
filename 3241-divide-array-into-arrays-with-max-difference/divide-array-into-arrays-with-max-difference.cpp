d2a3486f3e0f26c85108c8401991c2cf78a525e5
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-06-18 15:28:25 +0530

    Time: 109 ms (15.79%) | Memory: 135.7 MB (14.59%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    bool isPossible(vector<int>arr,int k){
+        if(abs(arr[2]-arr[0])>k) return false;
+        return true;
+    }
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        for (int i = 0; i < nums.size(); i += 3) {
-            if (nums[i + 2] - nums[i] > k) {
-                return {};
-            }
-            ans.push_back({nums[i], nums[i + 1], nums[i + 2]});
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>result;
+        for(int i=0;i<n-2;i=i+3){
+
+            vector<int>temp(nums.begin()+i,nums.begin()+i+3);
+
+            if(isPossible(temp,k))
+                result.push_back(temp);
+            else return {};
         }
-        return ans;
+        return result;
     }
 };
