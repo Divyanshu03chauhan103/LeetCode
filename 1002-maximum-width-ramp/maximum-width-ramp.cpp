cd595617a190eff0ac7ee9e041c76dc694f0235d
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-07-22 19:56:51 +0530

    Time: 40 ms (8.24%) | Memory: 48.5 MB (33.44%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>suffix(n,-1);
         
-        suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
+        int n=nums.size();
+        vector<int>indices(n,0);
+        for(int i=0;i<n;i++) indices[i]=i;
 
-            suffix[i]=max(suffix[i+1],nums[i]);
-        }
+        sort(indices.begin(),indices.end(),[&](int i,int j){
+            return nums[i]!=nums[j]?nums[i]<nums[j]:i<j;
+        });
 
         int ramp=0;
-        int j=0;
-        for(int i=0;i<n;i++){
-
-            while(j<n && nums[i]<=suffix[j]){
-                j++;
-            }
-            j--;
-            ramp=max(j-i,ramp);
-            
+        int min_ind=indices[0];
+        for(int i=1;i<n;i++){
+            min_ind=min(min_ind,indices[i]);
+            ramp=max(ramp,indices[i]-min_ind);
         }
-
         return ramp;
-    }
+        }
 };
