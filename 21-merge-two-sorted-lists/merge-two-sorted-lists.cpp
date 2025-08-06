087d1dc2f50fbe4884d0607dd6c46586a03dcd7b
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-08-07 00:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (29.44%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,18 +11,19 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode * Dnode = new ListNode();
-        ListNode* temp3=Dnode;
+        ListNode* Dnode=new ListNode();
+
         ListNode* temp1=list1;
         ListNode* temp2=list2;
+        ListNode* temp3=Dnode;
+
         while(temp1 && temp2){
-            
-            if(temp1->val>temp2->val){
-                temp3->next=temp2;
-                temp2=temp2->next;
-            }else{
+            if(temp1->val<temp2->val){
                 temp3->next=temp1;
                 temp1=temp1->next;
+            }else{
+                temp3->next=temp2;
+                temp2=temp2->next;;
             }
             temp3=temp3->next;
         }
@@ -33,7 +34,7 @@ public:
             temp3=temp3->next;
         }
         while(temp2){
-            temp3->next=temp2;;
+            temp3->next=temp2;
             temp2=temp2->next;
             temp3=temp3->next;
         }
