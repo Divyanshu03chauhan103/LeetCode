06d9d0f4ea248e76a9567b34e883e5c985d9cb02
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-08-11 20:26:44 +0530

    Time: 21 ms (57.45%) | Memory: 147.7 MB (50.53%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+       
+        vector<int>required;
+        int mod=1e9+7;
+        int N=n;
+        
+         while (N) {
+            int exp = floor(log2(N));  
+            int num = 1 << exp;        
+            N = N - num;               
+            required.push_back(num);   
+        }
+
+        reverse(required.begin(), required.end());
+        
+        vector<int> result;
+        for (auto &nums : queries) {
+            int left = nums[0];
+            int right = nums[1];
+            long long product = required[left];
+            for (int i = left+1; i <= right; i++) {
+                product = (product * required[i]) % mod;
+            }
+            result.push_back((int)product);
+        }
+        return result;
+    }
+};
+
