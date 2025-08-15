c138b2b22bc558dda430a8505eb50f7b2210aa56
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2025-08-15 12:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (75.31%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,62 +1,24 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&graph,vector<int>&sets,int node,int color){
-        sets[node] = color;
-        color = color==1? -1:1;
-        for(auto i:graph[node]){
-
-            if(sets[i]!=0 && sets[i]==sets[node]) return false;
-            else if(sets[i]!=0) continue;
-            else if(dfs(graph,sets,i,color) == false) return false;
-        }
-
-        return true;
-    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>sets(n,0);
-        int color=1;
+        vector<int>color(n,-1);
+
         for(int i=0;i<n;i++){
-            if(sets[i]!=0) continue;
-
-            if(dfs(graph,sets,i,color)==false) return false;
+            if(color[i]==-1 && !dfs(graph,color,1,i)){
+                return false;
+            }
         }
-
         return true;
     }
-};
-//bfs approach
-class Solution {
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>sets(n,0);
 
-        queue<int>q;
-        
-        for(int i=0;i<n;i++){
-        if(sets[i]!=0) continue;
-                q.push(i);
-                sets[i]=1;
-               
-                while(!q.empty()){
+    bool dfs(vector<vector<int>>&adj,vector<int>&color,int curr_color,int src){
+        color[src]=curr_color;
 
-                    int node=q.front();
-                    for(auto i:graph[node]){
-
-                        if(sets[i]!=0 && sets[node]==sets[i]){
-                            return false;
-                        }else if(sets[i]==0){
-
-                            sets[i]= sets[node]==1 ? -1:1; 
-                            q.push(i);
-                        }
-                    }
-               
-                    q.pop();
-                }
+        for(int i:adj[src]){
+            if(color[src]==color[i]) return false;
+            if(color[i]==-1 && !dfs(adj,color,1 - curr_color,i)) return false;
         }
-       
         return true;
     }
 };
