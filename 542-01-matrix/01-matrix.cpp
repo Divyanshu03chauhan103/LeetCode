9584b9f5b740edbb16307dd94ccb74d7cdf24372
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2026-01-13 03:45:05 +0530

    Time: 263 ms (5.01%) | Memory: 103.2 MB (5.07%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> result(n, vector<int>(m, 0));
-        vector<vector<int>> visited(n, vector<int>(m, 0));
+        
+        queue<vector<int>>q;
 
-        int Drow[] = {0, 1, 0, -1};
-        int Dcol[] = {1, 0, -1, 0};
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>dis(m,vector<int>(n,0));
 
-        queue<pair<pair<int,int>,int>>q;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
+        vector<vector<int>>visited(m,vector<int>(n,0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j,0});
                     visited[i][j]=1;
-                    q.push({{i,j},0});
                 }
             }
         }
 
-       while(!q.empty()){
+        vector<vector<int>>dir= {{1,0},{-1,0},{0,1},{0,-1}};
 
-        int row=q.front().first.first;
-        int col=q.front().first.second;
-        int dis=q.front().second;
+        while(!q.empty()){
 
-        result[row][col]=dis;
+            vector<int>curr=q.front();
+            int i=curr[0];
+            int j=curr[1];
+            int D=curr[2];
+            q.pop();
+            dis[i][j]=D;
+            for(vector<int>d:dir){
+                int new_i=i+d[0];
+                int new_j=j+d[1];
 
-        for(int i=0;i<4;i++){
-            int nrow=row+Drow[i];
-            int ncol = col+Dcol[i];
-
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0){
-                visited[nrow][ncol] = 1;
-                q.push({{nrow,ncol},dis+1});
-
+                if(new_i>=0 && new_j>=0 && new_i<m && new_j<n && !visited[new_i][new_j]) {
+                    q.push({new_i,new_j,D+1});
+                    visited[new_i][new_j]=1;
+                }
             }
 
         }
 
-        q.pop();
-
-       }
-
-        return result;
+        return dis;
     }
 };
