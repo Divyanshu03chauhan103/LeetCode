92852aa8710d8395c9d9f6d42cf6df89f2f6b208
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2026-03-05 21:57:31 +0530

    Time: 1 ms (27.73%) | Memory: 9.3 MB (95.63%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int open = 1;     // pattern 1010
+        int closed = 0;   // pattern 0101
+
+        int result1 = 0;
+        int result2 = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(s[i] != (open ? '1' : '0'))
+                result1++;
+
+            if(s[i] != (closed ? '1' : '0'))
+                result2++;
+
+            open ^= 1;
+            closed ^= 1;
+        }
+
+        return min(result1, result2);
+    }
+};
