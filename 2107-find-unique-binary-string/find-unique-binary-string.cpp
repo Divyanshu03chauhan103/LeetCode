041a5e733e7c067317d3c1e8f96d0e8f9cb3ed21
divvu03 <122027082+Divyanshu03chauhan103@users.noreply.github.com>
2026-03-08 15:02:07 +0530

    Time: 20 ms (12.83%) | Memory: 22.1 MB (12.96%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -3,16 +3,21 @@ public:
     void solve(unordered_set<string>s,string &ans,int n,string &curr){
 
         if(curr.size()==n){
+            //checks whether the string is present in the ans or not 
             if(s.find(curr)==s.end())
                 ans = curr;
             return;
         }
+        // if ans is found then no need to check further
         if(ans != "") return ;
+        
         curr.push_back('0');
         solve(s,ans,n,curr);
+        //backtrack
         curr.pop_back();
         curr.push_back('1');
         solve(s,ans,n,curr);
+        //backtrack
         curr.pop_back();
     }
     string findDifferentBinaryString(vector<string>& nums) {
